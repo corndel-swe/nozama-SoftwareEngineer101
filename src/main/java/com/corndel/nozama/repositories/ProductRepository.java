2c5541be3410f696b3730665172f63c47af8fb5f
Brown1ie <edward.a.brown@icloud.com>
2025-10-13 15:54:33 +0100

    1-get-all-products

--- src/main/java/com/corndel/nozama/repositories/ProductRepository.java
+++ src/main/java/com/corndel/nozama/repositories/ProductRepository.java
@@ -1,6 +1,12 @@
 package com.corndel.nozama.repositories;
 
+import com.corndel.nozama.DB;
+import com.corndel.nozama.models.Product;
+import com.corndel.nozama.models.User;
+
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductRepository {
     public static String  addProduct (String name, String description, float price, int stock, String url ) throws SQLException {
@@ -9,11 +15,33 @@ public class ProductRepository {
 
         var query = "INSERT INTO products (name,description,price,stockQuantity, imageURL) VALUES("+name+ "," +description+ "," +price+ ","+stock+ "," +url+ ")";
 
-        return query;
-
-
-
-
-
+        try (var con = DB.getConnection();
+             var stmt = con.createStatement();
+             var rs = stmt.executeQuery(query)){
+            return rs.toString();
+        }
 }
+    public static List<Product> findAll() throws SQLException {
+        var query = "SELECT id, name, description, price, stockQuantity, imageURL FROM products";
+
+        try (var con = DB.getConnection();
+             var stmt = con.createStatement();
+             var rs = stmt.executeQuery(query)) {
+
+            var products = new ArrayList<Product>();
+            while (rs.next()) {
+                var id = rs.getInt("id");
+                var name = rs.getString("name");
+                var description = rs.getString("description");
+                var price = rs.getFloat("price");
+                var stockQuantity = rs.getInt("stockQuantity");
+                var imageURL = rs.getString("imageURL");
+
+                products.add(new Product(id, name, description, price, stockQuantity, imageURL));
+            }
+            return products;
+        }
+    };
+
+
 };
