91cbd155a4856a3e578c9d68b476cbbf2a8dee80
SoftwareEngineer101 <tanianazarian125@gmail.com>
2025-10-13 15:56:40 +0100

    2-get-single-product

--- src/main/java/com/corndel/nozama/repositories/ProductRepository.java
+++ src/main/java/com/corndel/nozama/repositories/ProductRepository.java
@@ -1,19 +1,37 @@
 package com.corndel.nozama.repositories;
 
+import com.corndel.nozama.DB;
+import com.corndel.nozama.models.Product;
 import java.sql.SQLException;
 
 public class ProductRepository {
-    public static String  addProduct (String name, String description, float price, int stock, String url ) throws SQLException {
-        String json = "{ \"name\" :" + name +",\"description\" :" + description + ",\"price\" :" + price + ",\"stockQuantity\" :" + stock + ",\"imageURL\" :" + url+"}";
+    public static String addProduct(String name, String description, float price, int stock, String url) throws SQLException {
+        String json = "{ \"name\" :" + name + ",\"description\" :" + description + ",\"price\" :" + price + ",\"stockQuantity\" :" + stock + ",\"imageURL\" :" + url + "}";
 
-
-        var query = "INSERT INTO products (name,description,price,stockQuantity, imageURL) VALUES("+name+ "," +description+ "," +price+ ","+stock+ "," +url+ ")";
+        var query = "INSERT INTO products (name,description,price,stockQuantity, imageURL) VALUES(" + name + "," + description + "," + price + "," + stock + "," + url + ")";
 
         return query;
+    }
 
 
+    public static Product findById(int id) throws SQLException {
 
+        var querySingleID = "SELECT * FROM users WHERE id =" + id;
 
+        try (var con = DB.getConnection();
+             var stmt = con.createStatement();
+             var rs = stmt.executeQuery(querySingleID);) {
 
-}
+            var name = rs.getString("name");
+            var description = rs.getString("description");
+            var price = rs.getFloat("price");
+            var stock = rs.getInt("stockQuantity");
+            var url = rs.getString("imageURL");
+
+            return new Product(id,name,description,price,stock,url);
+
+        }
+
+    }
+
 };
