468cca5cb2cfb451bf9184a063400291c911a6f8
SoftwareEngineer101 <tanianazarian125@gmail.com>
2025-10-14 16:01:48 +0100

    productendpointdebug

--- src/main/java/com/corndel/nozama/repositories/ProductRepository.java
+++ src/main/java/com/corndel/nozama/repositories/ProductRepository.java
@@ -4,6 +4,8 @@ import com.corndel.nozama.DB;
 import com.corndel.nozama.models.Product;
 import com.corndel.nozama.models.User;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,21 +14,34 @@ public class ProductRepository {
     public static Product addProduct(String name, String description, float price, int stock, String url) throws SQLException {
         String json = "{ \"name\" :" + name + ",\"description\" :" + description + ",\"price\" :" + price + ",\"stockQuantity\" :" + stock + ",\"imageURL\" :" + url + "}";
 
-        var query = "INSERT INTO products (name,description,price,stockQuantity, imageURL) VALUES(" + name + "," + description + "," + price + "," + stock + "," + url + ")";
+//        var query = "INSERT INTO products (name,description,price,stockQuantity, imageURL) VALUES(" + name + "," + description + "," + price + "," + stock + "," + url + ")";
+        var query = "INSERT INTO products (name,description,price,stockQuantity, imageURL) VALUES (?,?,?,?,?)";
 
-        try (var con = DB.getConnection();
-             var stmt = con.createStatement();
-             var rs = stmt.executeQuery(query)){
-            var id = rs.getInt("id");
-            return new Product(id, name, description, price, stock, url);
-        }
+        try (Connection connection = DB.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query))
+
+
+
+        { preparedStatement.setString(1, name);
+            preparedStatement.setString(2, description);
+            preparedStatement.setFloat(3, price);
+            preparedStatement.setInt(4, stock);
+            preparedStatement.setString(5, url);
+
+            try ( var rs = preparedStatement.executeQuery(query)) {
+
+                var id = rs.getInt("id");
+                return new Product(id, name, description, price, stock, url);}
+            }
+
 }
+
+
     public static List<Product> findAll() throws SQLException {
         var query = "SELECT id, name, description, price, stockQuantity, imageURL FROM products";
-
-        try (var con = DB.getConnection();
-             var stmt = con.createStatement();
-             var rs = stmt.executeQuery(query)) {
+            try (var con = DB.getConnection();
+                 var stmt = con.createStatement();
+                 var rs = stmt.executeQuery(query)){
 
             var products = new ArrayList<Product>();
             while (rs.next()) {
