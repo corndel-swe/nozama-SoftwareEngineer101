f76299fdc2085ec1645a79b2118042120b86cdae
SoftwareEngineer101 <tanianazarian125@gmail.com>
2025-10-16 12:04:51 +0100

    Completed Exercise 3

--- src/main/java/com/corndel/nozama/exercises/D3E2.java
+++ src/main/java/com/corndel/nozama/exercises/D3E2.java
@@ -1,5 +1,11 @@
 package com.corndel.nozama.exercises;
 
+import io.javalin.http.BadRequestResponse;
+import io.javalin.http.ForbiddenResponse;
+import io.javalin.http.UnauthorizedResponse;
+
+import java.util.Objects;
+
 public class D3E2 {
 
   public class Account {
@@ -29,13 +35,22 @@ public class D3E2 {
      */
     public void updateUsername(String newUsername, String passwordAttempt) throws Exception {
       // TODO: If newUsername is not given, throw a BadRequestResponse
+      if (newUsername == null) {
+        throw new BadRequestResponse("Please enter a name");
+      }
 
       // TODO: If passwordAttempt is not given, throw an UnauthorizedResponse
+      if (passwordAttempt == null) {
+        throw new UnauthorizedResponse("Please enter password");}
 
-      // TODO: If passwordAttempt is given but not correct, throw a ForbiddenResponse
+        // TODO: If passwordAttempt is given but not correct, throw a ForbiddenResponse
+      if (!Objects.equals(passwordAttempt, password)) {
+        throw new ForbiddenResponse("Password entered is incorrect");}
 
-      // TODO: If newUsername is given and passwordAttempt is correct, update the
-      // username
-    }
-  }
+          // TODO: If newUsername is given and passwordAttempt is correct, update the
+          // username
+      if (Objects.equals(passwordAttempt, password) && !Objects.equals(newUsername, "")) {
+        username = newUsername;}
+        }
+      }
 }
